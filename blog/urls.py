32897f46f9e78334951b3b1a7bf1798339d88358
arianrhod <pataki.annamaria@hotmail.com>
2018-04-04 22:11:37 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views	#az összes view-t importáljuk
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),	# itt egy post_list nevű viewt rendeltünk hozzá a ^$ URL-hez. Azaz azért az üres(^$) regex, mert ha valaki a 'http://127.0.0.1:8000/' címen lép be a weboldaladra. Utána ezzel tudja h a views.post_list a helyes lépés. Ezt keresse. name='post_list' az URL neve, amit a view azonosítására használunk.
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
